e80302684701894814de071bed9f75a05318360d
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2024-08-21 13:00:13 -0400

    Update create-hash.js

--- create-hash.js
+++ create-hash.js
@@ -4,6 +4,6 @@ const { createHmac } = require('node:crypto');
 
 const secret = 'abcdefg'; // This key is used in the HMAC algorithm to produce the hash.
 const hash = createHmac('sha256', secret) // Initializes the HMAC algorithm
-               .update('I love cupcakes') // Adds the data you want to hash to the HMAC object
-               .digest('hex'); // Finalizes the HMAC computation and returns the resulting hash in hexadecimal format
+    .update('I love cupcakes') // Adds the data you want to hash to the HMAC object
+    .digest('hex'); // Finalizes the HMAC computation and returns the resulting hash in hexadecimal format
 console.log(hash);
